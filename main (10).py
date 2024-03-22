fb09af024d5624546548ecf6f7aeb7bde2f150e4
car200331 <163993028+car200331@users.noreply.github.com>
2024-03-22 10:16:19 +0300

    задача 1

---
+++ main (10).py
@@ -0,0 +1,5 @@
+#coding: utf-8
+number1 = float(input("Введите первое число: "))
+number2 = float(input("Введите второе число: "))
+max_number = max(number1, number2)
+print("Наибольшее число: ", max_number)
