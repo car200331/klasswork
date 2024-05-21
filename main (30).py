b20a2ab9ebd2833ca18dc509a01bab32d08006cc
car200331 <163993028+car200331@users.noreply.github.com>
2024-05-21 13:39:07 +0300

    21.05 задача 3

---
+++ main (30).py
@@ -0,0 +1,9 @@
+#coding: utf-8
+import math
+def main():
+    for i in range(1, 21):
+        exp_value = math.exp(i)
+        print(f"e^{i} = {exp_value}")
+
+if __name__ == "__main__":
+    main()
